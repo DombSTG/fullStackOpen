8095693799e1069f4fa33c7a7766e927a879e083
Domb <viniciusdombidau@gmail.com>
2026-07-17 03:12:20 -0300

    Exercicio 1.7 Realizado

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,15 +16,44 @@ const Totais = ({ texto, quant }) => {
     );
 };
 
+const Total = ({ total }) => <p>all {total}</p>;
+
+const Avg = ({ aver }) => {
+    return <p>average {aver}</p>;
+};
+
+const Porcent = ({ posi }) => <p>positive {posi} </p>
+
 const App = () => {
     // salve os cliques de cada botão em seu próprio estado
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [click, setClick] = useState(0);
 
-    const handleGood = () => setGood(good + 1);
-    const handleNeutral = () => setNeutral(neutral + 1);
-    const handleBad = () => setBad(bad + 1);
+    const handleGood = () => {
+        setGood(good + 1);
+        setTotal(total + 1);
+        setClick(click + 1);
+    };
+    const handleNeutral = () => {
+        setNeutral(neutral + 1);
+        setTotal(total + 1);
+    };
+    const handleBad = () => {
+        setBad(bad + 1);
+        setTotal(total + 1);
+        setClick(click - 1);
+    };
+
+    const aver = total === 0 ? 0 : click / total;
+
+    const porcent = (good / total) * 100
+    
+    const retornPorcentagem = total === 0 ? 0 : porcent
+    
+
 
     return (
         <div>
@@ -36,6 +65,9 @@ const App = () => {
             <Totais texto="good" quant={good} />
             <Totais texto="neutral" quant={neutral} />
             <Totais texto="bad" quant={bad} />
+            <Total total={total} />
+            <Avg aver={aver} />
+            <Porcent posi={retornPorcentagem} />
         </div>
     );
 };
