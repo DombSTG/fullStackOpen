53bbd4b8d477c415ffdd1a289d0b2b788c23eb36
Domb <viniciusdombidau@gmail.com>
2026-07-17 23:59:41 -0300

    Exercicio 1.8 Concluido

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -22,7 +22,25 @@ const Avg = ({ aver }) => {
     return <p>average {aver}</p>;
 };
 
-const Porcent = ({ posi }) => <p>positive {posi} </p>
+const Porcent = ({ posi }) => <p>positive {posi} </p>;
+
+const Statistics = ({ good, neutral, bad, all, pontos}) => {
+    const aver = all === 0 ? 0 : pontos / all;
+
+    const porcent = (good / all) * 100;
+
+    const retornPorcentagem = all === 0 ? 0 : porcent;
+
+    return (
+    <>
+    <Totais texto="good" quant={good} />
+    <Totais texto="neutral" quant={neutral} />
+    <Totais texto="bad" quant={bad} />
+    <Total total={all} />
+    <Avg aver={aver} />
+    <Porcent posi={retornPorcentagem} />
+    </>)
+};
 
 const App = () => {
     // salve os cliques de cada botão em seu próprio estado
@@ -30,12 +48,12 @@ const App = () => {
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
     const [total, setTotal] = useState(0);
-    const [click, setClick] = useState(0);
+    const [pontos, setPontos] = useState(0);
 
     const handleGood = () => {
         setGood(good + 1);
         setTotal(total + 1);
-        setClick(click + 1);
+        setPontos(pontos + 1);
     };
     const handleNeutral = () => {
         setNeutral(neutral + 1);
@@ -44,16 +62,8 @@ const App = () => {
     const handleBad = () => {
         setBad(bad + 1);
         setTotal(total + 1);
-        setClick(click - 1);
+        setPontos(pontos - 1);
     };
-
-    const aver = total === 0 ? 0 : click / total;
-
-    const porcent = (good / total) * 100
-    
-    const retornPorcentagem = total === 0 ? 0 : porcent
-    
-
 
     return (
         <div>
@@ -62,12 +72,7 @@ const App = () => {
             <Botao clicar={handleNeutral} texto="neutral" />
             <Botao clicar={handleBad} texto="bad" />
             <h2>statistics</h2>
-            <Totais texto="good" quant={good} />
-            <Totais texto="neutral" quant={neutral} />
-            <Totais texto="bad" quant={bad} />
-            <Total total={total} />
-            <Avg aver={aver} />
-            <Porcent posi={retornPorcentagem} />
+            <Statistics good={good} neutral={neutral} bad={bad} all={total} pontos={pontos}/>
         </div>
     );
 };
