03ab45826d644f07f138dbaf5010900a9c1f81bf
Domb <viniciusdombidau@gmail.com>
2026-07-18 00:44:38 -0300

    Exercicio 1.10 Concluido

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,42 +6,31 @@ const Botao = ({ clicar, texto }) => {
     return <button onClick={clicar}>{texto}</button>;
 };
 
-const Totais = ({ texto, quant }) => {
-    return (
-        <div>
-            <p>
-                {texto} {quant}
-            </p>
-        </div>
-    );
-};
+const StatisticsLine = ({ text, value }) => (
+    <p>
+        {text} {value}
+    </p>
+);
 
-const Total = ({ total }) => <p>all {total}</p>;
+const Statistics = ({ good, neutral, bad, pontos }) => {
+    const total = good + neutral + bad;
+    const aver = total === 0 ? 0 : pontos / total;
 
-const Avg = ({ aver }) => {
-    return <p>average {aver}</p>;
-};
+    const porcent = (good / total) * 100;
 
-const Porcent = ({ posi }) => <p>positive {posi} </p>;
+    const retornPorcentagem = total === 0 ? 0 : porcent;
 
-const Statistics = ({ good, neutral, bad, all, pontos }) => {
-    const aver = all === 0 ? 0 : pontos / all;
-
-    const porcent = (good / all) * 100;
-
-    const retornPorcentagem = all === 0 ? 0 : porcent;
-
-    if (all === 0) {
+    if (total === 0) {
         return <div>Clique em um dos botões para usar a aplicação!</div>;
     }
     return (
         <>
-            <Totais texto="good" quant={good} />
-            <Totais texto="neutral" quant={neutral} />
-            <Totais texto="bad" quant={bad} />
-            <Total total={all} />
-            <Avg aver={aver} />
-            <Porcent posi={retornPorcentagem} />
+            <StatisticsLine text="good" value={good} />
+            <StatisticsLine text="neutral" value={neutral} />
+            <StatisticsLine text="bad" value={bad} />
+            <StatisticsLine text="all" value={total} />
+            <StatisticsLine text="average" value={aver} />
+            <StatisticsLine text="positive" value={retornPorcentagem + " %"} />
         </>
     );
 };
@@ -51,21 +40,17 @@ const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-    const [total, setTotal] = useState(0);
     const [pontos, setPontos] = useState(0);
 
     const handleGood = () => {
         setGood(good + 1);
-        setTotal(total + 1);
         setPontos(pontos + 1);
     };
     const handleNeutral = () => {
         setNeutral(neutral + 1);
-        setTotal(total + 1);
     };
     const handleBad = () => {
         setBad(bad + 1);
-        setTotal(total + 1);
         setPontos(pontos - 1);
     };
 
@@ -80,7 +65,6 @@ const App = () => {
                 good={good}
                 neutral={neutral}
                 bad={bad}
-                all={total}
                 pontos={pontos}
             />
         </div>
